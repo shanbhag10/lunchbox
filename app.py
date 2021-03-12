b690def76593fa3087b2c54525ed211de3fc9a0f
Saurabh Shanbhag <saurabh.v.shanbhag@gmail.com>
2021-03-11 21:00:09 -0800

    Add sample db app

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+#from send_mail import send_mail
+
+app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:LionelMessi10@localhost/lunchbox'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gqmmaaoqgiciyp:fabafe7ebf913d0c92758b4a6b796c1dac6829ef98a9a46d09cefbbdace16e6c@ec2-54-162-119-125.compute-1.amazonaws.com:5432/dfrjifr5q232an'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        # print(customer, dealer, rating, comments)
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields')
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            #send_mail(customer, dealer, rating, comments)
+            return render_template('success.html')
+        return render_template('index.html', message='You have already submitted feedback')
+
+
+if __name__ == '__main__':
+    app.run()
